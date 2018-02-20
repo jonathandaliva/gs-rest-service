fed4074a170abef97dc98fb2f44bfc17d48db972
Jon <jonathan.daliva@gmail.com>
2018-02-20 15:51:25 -0500

    remove spaces

--- gs-rest-service2/src/main/java/hello/GreetingController.java
+++ gs-rest-service2/src/main/java/hello/GreetingController.java
@@ -18,63 +18,63 @@ import org.apache.http.impl.client.HttpClientBuilder;
 @RestController
 public class GreetingController {
 
-    private final AtomicLong counter = new AtomicLong();
+	private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="lat", defaultValue="38.803328") String lat, @RequestParam(value="lon", defaultValue="-77.039026") String lon, ) {
-    	//38.803328, -77.039026
-    	//https://api.weather.gov/points/38.803328,-77.039026/forecast
-    	String data="";
-    	
-    	//call api, parse json, create simple one.
-    	
-    	HttpClient client = HttpClientBuilder.create().build();
-    	String url = "https://api.weather.gov/points/"+lat+","+lon+"/forecast";
-    	HttpGet httpget = new HttpGet(url);
-    	
-    	System.out.println("executing request " + httpget.getURI());
-    	HttpResponse response = client.execute(httpget);
-        final int statusCode = response.getStatusLine().getStatusCode();
-        
-        Greeting greetingResponse = new Greeting(counter.incrementAndGet())
-        if (statusCode != HttpStatus.SC_OK) {
-        	System.out.println( "Error " + statusCode + " for URL " + url);
-        } else {
-        	System.out.println("----------------------------------------");
-        	System.out.println(response.toString());
-        	System.out.println("----------------------------------------");
-        	
-        	HttpEntity getResponseEntity = response.getEntity();
-        	
-        	Gson gson = new Gson();
-        	
-        	Reader reader = new InputStreamReader(getResponseEntity.getContent());
-        	
-        	WeatherResponse parsedresponse = gson.fromJson(reader, WeatherResponse.class);
-        	WeatherPeriods objPeriod = parsedresponse.properties.periods.get(0);
-	        System.out.println("today Desc: " + String.valueOf(objPeriod.shortForecast));
-	        System.out.println("today high: " + String.valueOf(objPeriod.temperature));
-	        greetingResponse.setTdyDesc(objPeriod.shortForecast);
-	        greetingResponse.setTdyHigh(objPeriod.temperature);
-	        
-	        objPeriod = parsedresponse.properties.periods.get(1);
-	        System.out.println("today low: " + String.valueOf(objPeriod.temperature));
-	        greetingResponse.setTdyLow(objPeriod.temperature);
-	        
-	        objPeriod = parsedresponse.properties.periods.get(2);
-	        System.out.println("tomorrow Desc: " + String.valueOf(objPeriod.shortForecast));
-	        System.out.println("tomorrow high: " + String.valueOf(objPeriod.temperature));
-	        greetingResponse.setTmwDesc(objPeriod.shortForecast);
-	        greetingResponse.setTmwHigh(objPeriod.temperature);
-	        
-	        objPeriod = parsedresponse.properties.periods.get(3);
-	        System.out.println("tomorrow low: " + String.valueOf(objPeriod.temperature));
-	        greetingResponse.setTmwLow(objPeriod.temperature);
-	        
-	        System.out.println("------------------DONE----------------------");
-        	
-        }
-        
-        return greetingResponse;
-    }
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value="lat", defaultValue="38.803328") String lat, @RequestParam(value="lon", defaultValue="-77.039026") String lon, ) {
+		//38.803328, -77.039026
+		//https://api.weather.gov/points/38.803328,-77.039026/forecast
+		String data="";
+		
+		//call api, parse json, create simple one.
+		
+		HttpClient client = HttpClientBuilder.create().build();
+		String url = "https://api.weather.gov/points/"+lat+","+lon+"/forecast";
+		HttpGet httpget = new HttpGet(url);
+		
+		System.out.println("executing request " + httpget.getURI());
+		HttpResponse response = client.execute(httpget);
+		final int statusCode = response.getStatusLine().getStatusCode();
+		
+		Greeting greetingResponse = new Greeting(counter.incrementAndGet())
+		if (statusCode != HttpStatus.SC_OK) {
+			System.out.println( "Error " + statusCode + " for URL " + url);
+		} else {
+			System.out.println("----------------------------------------");
+			System.out.println(response.toString());
+			System.out.println("----------------------------------------");
+			
+			HttpEntity getResponseEntity = response.getEntity();
+			
+			Gson gson = new Gson();
+			
+			Reader reader = new InputStreamReader(getResponseEntity.getContent());
+			
+			WeatherResponse parsedresponse = gson.fromJson(reader, WeatherResponse.class);
+			WeatherPeriods objPeriod = parsedresponse.properties.periods.get(0);
+			System.out.println("today Desc: " + String.valueOf(objPeriod.shortForecast));
+			System.out.println("today high: " + String.valueOf(objPeriod.temperature));
+			greetingResponse.setTdyDesc(objPeriod.shortForecast);
+			greetingResponse.setTdyHigh(objPeriod.temperature);
+			
+			objPeriod = parsedresponse.properties.periods.get(1);
+			System.out.println("today low: " + String.valueOf(objPeriod.temperature));
+			greetingResponse.setTdyLow(objPeriod.temperature);
+			
+			objPeriod = parsedresponse.properties.periods.get(2);
+			System.out.println("tomorrow Desc: " + String.valueOf(objPeriod.shortForecast));
+			System.out.println("tomorrow high: " + String.valueOf(objPeriod.temperature));
+			greetingResponse.setTmwDesc(objPeriod.shortForecast);
+			greetingResponse.setTmwHigh(objPeriod.temperature);
+			
+			objPeriod = parsedresponse.properties.periods.get(3);
+			System.out.println("tomorrow low: " + String.valueOf(objPeriod.temperature));
+			greetingResponse.setTmwLow(objPeriod.temperature);
+			
+			System.out.println("------------------DONE----------------------");
+			
+		}
+		
+		return greetingResponse;
+	}
 }
